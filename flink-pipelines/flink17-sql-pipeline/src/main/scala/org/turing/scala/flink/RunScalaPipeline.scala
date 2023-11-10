b6053833d679435fc72a3dee8e7233251744ee8c
Tony <economic_luojie@outlook.com>
2023-11-10 17:37:42 +0800

    feat: add flink18-sql-pipeline

--- flink-pipelines/flink17-sql-pipeline/src/main/scala/org/turing/scala/flink/RunScalaPipeline.scala
+++ flink-pipelines/flink17-sql-pipeline/src/main/scala/org/turing/scala/flink/RunScalaPipeline.scala
@@ -1,14 +1,21 @@
 package org.turing.scala.flink
 
-import org.slf4j.{Logger, LoggerFactory}
+import org.slf4j.LoggerFactory
 
-object TuringFlinkPipelineApp {
+/**
+ * @descri: pipeline入口
+ *
+ * @author: lj.michale
+ * @date: 2023/11/10 17:27
+ */
+object RunScalaPipeline {
 
   private val logger = LoggerFactory.getLogger(this.getClass)
 
   def main(args: Array[String]): Unit = {
 
     logger.info(" #################################### ")
+
   }
 
 
