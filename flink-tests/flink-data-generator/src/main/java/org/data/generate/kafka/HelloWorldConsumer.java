7dac28330b54bab0c2d52e9334be2f2d968fde4f
Tony <economic_luojie@outlook.com>
2023-11-14 18:22:16 +0800

    feat: kafka producer and consumer

---
+++ flink-tests/flink-data-generator/src/main/java/org/data/generate/kafka/HelloWorldConsumer.java
@@ -0,0 +1,45 @@
+package org.data.generate.kafka;
+
+import java.util.Arrays;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import java.time.Duration;
+
+/**
+ * @descri:
+ *
+ * @author: lj.michale
+ * @date: 2023/11/14 18:14
+ */
+public class HelloWorldConsumer {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.43.102:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG ,"kafeidou_group") ;
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put("auto.offset.reset", "earliest");
+
+        Consumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("kafeidou"));
+
+        while (true) {
+            long timeout = 1000;
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(timeout));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+            }
+        }
+
+    }
+
+}
