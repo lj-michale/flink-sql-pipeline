c54a8d93800392d9e9f200cc52ef813a01487bf6
Tony <economic_luojie@outlook.com>
2023-11-16 15:27:07 +0800

    feat: turing-common-springboot-starter|线程池拒绝策略代理执行器

---
+++ flink-frameworks/turing-common-springboot-starter/src/main/java/org/turing/common/threadpool/proxy/RejectedProxyInvocationHandler.java
@@ -0,0 +1,41 @@
+package org.turing.common.threadpool.proxy;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @descri: 线程池拒绝策略代理执行器
+ *
+ * @author: lj.michale
+ * @date: 2023/11/16 15:26
+ */
+@Slf4j
+@AllArgsConstructor
+public class RejectedProxyInvocationHandler implements InvocationHandler {
+
+    /**
+     * Target object
+     */
+    private final Object target;
+
+    /**
+     * Reject count
+     */
+    private final AtomicLong rejectCount;
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        rejectCount.incrementAndGet();
+        try {
+            log.error("线程池执行拒绝策略, 此处模拟报警...");
+            return method.invoke(target, args);
+        } catch (InvocationTargetException ex) {
+            throw ex.getCause();
+        }
+    }
+}
