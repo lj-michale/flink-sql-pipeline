d4ee1f5a2b2d4be1a01c2cdbeffe41404f7ca563
Tony <economic_luojie@outlook.com>
2023-11-16 15:30:31 +0800

    feat: turing-common-springboot-starter|快速消费线程池

---
+++ flink-frameworks/turing-common-springboot-starter/src/main/java/org/turing/common/support/eager/EagerThreadPoolExecutor.java
@@ -0,0 +1,60 @@
+package org.turing.common.support.eager;
+
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @descri: 快速消费线程池
+ *
+ * @author: lj.michale
+ * @date: 2023/11/16 15:28
+ */
+public class EagerThreadPoolExecutor extends ThreadPoolExecutor {
+
+    public EagerThreadPoolExecutor(int corePoolSize,
+                                   int maximumPoolSize,
+                                   long keepAliveTime,
+                                   TimeUnit unit,
+                                   TaskQueue<Runnable> workQueue,
+                                   ThreadFactory threadFactory,
+                                   RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
+    }
+
+    private final AtomicInteger submittedTaskCount = new AtomicInteger(0);
+
+    public int getSubmittedTaskCount() {
+        return submittedTaskCount.get();
+    }
+
+    @Override
+    protected void afterExecute(Runnable r, Throwable t) {
+        submittedTaskCount.decrementAndGet();
+    }
+
+    @Override
+    public void execute(Runnable command) {
+        submittedTaskCount.incrementAndGet();
+        try {
+            super.execute(command);
+        } catch (RejectedExecutionException ex) {
+            TaskQueue taskQueue = (TaskQueue) super.getQueue();
+            try {
+                if (!taskQueue.retryOffer(command, 0, TimeUnit.MILLISECONDS)) {
+                    submittedTaskCount.decrementAndGet();
+                    throw new RejectedExecutionException("Queue capacity is full.", ex);
+                }
+            } catch (InterruptedException iex) {
+                submittedTaskCount.decrementAndGet();
+                throw new RejectedExecutionException(iex);
+            }
+        } catch (Exception ex) {
+            submittedTaskCount.decrementAndGet();
+            throw ex;
+        }
+    }
+}
