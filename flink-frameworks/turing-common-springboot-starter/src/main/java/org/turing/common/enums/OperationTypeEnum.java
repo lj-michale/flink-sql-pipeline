4f57e92f197ecf5b09bc156138d70380947f6a48
Tony <economic_luojie@outlook.com>
2023-11-16 15:22:51 +0800

    feat: turing-common-springboot-starter|操作类型

---
+++ flink-frameworks/turing-common-springboot-starter/src/main/java/org/turing/common/enums/OperationTypeEnum.java
@@ -0,0 +1,16 @@
+package org.turing.common.enums;
+
+/**
+ * @descri: 操作类型
+ *
+ * @author: lj.michale
+ * @date: 2023/11/16 15:22
+ */
+public enum OperationTypeEnum {
+
+    SAVE,
+
+    UPDATE,
+
+    DELETE
+}
